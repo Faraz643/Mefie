ec490da8e11e92b8953ae5ee3660bd8ffadb227f
__Faraz__ <92649053+Faraz643@users.noreply.github.com>
2026-09-15 10:35:26 +0530

    Add persistent tab navigator

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { display: "none" },
+        animation: "none",
+      }}
+    >
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="events" />
+      <Tabs.Screen name="you" />
+    </Tabs>
+  );
+}
